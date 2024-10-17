7b52023dfcbde5580f2a658b0c3c432162610db1
MAHESH LANKAVALASA <mightymahesh18311m026@gmail.com>
2024-10-17 16:18:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { differenceInYears } from 'date-fns';
+import Home from './HomePage'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HomePage/index.js
@@ -0,0 +1,56 @@
+import {useState} from 'react'
+import './index.css'
+const Home=()=>{
+    const { getYear}=require('date-fns')
+    const [user,setUser]=useState('')
+    const [eligible,setEligible]=useState(0)
+    const [select,setSelect]=useState(false)
+    const [disable,setDisable]=useState(true)
+
+    console.log(user)
+    
+    const getData=(event)=>{
+    event.preventDefault()
+    const year=getYear(user)
+    const myAge=new Date().getFullYear()
+    const diff=myAge-year
+    if (year!==null){
+        setSelect(true)
+    }
+    setEligible(diff)
+    }
+    const changed=(event)=>{
+        setUser(event.target.value)
+        setDisable(false)
+        
+    }
+    
+
+    return(
+        <div className='mainDiv'>
+            <h1>Election Department of India </h1>
+            <label htmlFor='date'>Select Your Birth Date</label>
+            <form onSubmit={getData}>
+                <input id="date" onChange={changed} type="Date"/>
+                <div>
+                    <button disabled={disable} type="submit">Check</button>
+                </div>
+                {select? 
+                    eligible>18 ?
+                    <div> 
+                        <p className=' eligible text'>You are eligible to vote  </p> 
+                        <p className='message'>If you have a voter id you can surely vote to your favorite leader but think twice before choosing your leader</p>
+                    </div>
+                     : 
+                    <div>
+                         <p className='text notEligible'>You are not eligible to vote </p>
+                         <p className='message'>So sad that you are not over 18. You have to wait for some more years.</p>
+
+                    </div> 
+                     : null}
+            </form>
+        </div>
+    )
+}
+
+export default Home
